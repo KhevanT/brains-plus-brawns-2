fa77b727c2ffd40204a3dfa82c001729aab0bbd5
Khevan Thanki <Khevanyt@gmail.com>
2024-03-01 19:45:36 +0530

    incomplete gate system

--- Brains Plus Brawns 2/Assets/Scripts/DialogueTrigger.cs
+++ Brains Plus Brawns 2/Assets/Scripts/DialogueTrigger.cs
@@ -13,10 +13,27 @@ public class DialogueLine
 
 public class DialogueTrigger : MonoBehaviour
 {
+    public Kingdom locationKingdom;
+    public bool isGateKeeper = false;
+    public bool isPasswordCorrect = false;
     [SerializeField] public List<DialogueLine> dialogue;
 
     public void TriggerDialogue()
     {
-        GameObject.FindGameObjectWithTag("Dialogue Manager").GetComponent<DialogueManager>().StartDialogue(dialogue);
+        StartCoroutine(GameObject.FindGameObjectWithTag("Dialogue Manager").GetComponent<DialogueManager>().StartDialogue(this, dialogue));
+    }
+
+    public GameObject GetPasswordObject()
+    {
+        if(isGateKeeper)
+            return this.transform.GetChild(2).gameObject;
+        else
+            return null;
+    }
+
+    void Start()
+    {
+        if(isGateKeeper)
+            GetPasswordObject().SetActive(false);
     }
 }
